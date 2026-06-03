eb932c29f50353ab3674a417146b4101c50d9165
najhichafikx-sketch <najhichafikx@gmail.com>
2026-06-03 14:25:11 -0400

    Disable Auto Ads, add highperformanceformat.com to CSP

--- components/AdScript.js
+++ components/AdScript.js
@@ -1,26 +1,3 @@
-'use client';
-
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-
-const AD_URL = 'https://pl29606008.effectivecpmnetwork.com/c2/dc/af/c2dcaf12d7d28b9604b8957217fbf8d4.js';
-
 export default function AdScript() {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const id = 'adsterra-auto-ads';
-
-    const existing = document.getElementById(id);
-    if (existing) existing.remove();
-
-    const script = document.createElement('script');
-    script.id = id;
-    script.src = AD_URL;
-    script.async = true;
-    script.setAttribute('data-cfasync', 'false');
-    document.body.appendChild(script);
-  }, [pathname]);
-
   return null;
 }
--- proxy.ts
+++ proxy.ts
@@ -31,7 +31,7 @@ const HTML_REDIRECTS = {
 
 const CSP = [
   "default-src 'self'",
-   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.adsterra.com https://*.effectivecpmnetwork.com https://*.adsterratrk.com https://*.adsterra.biz https://www.googletagmanager.com https://www.google-analytics.com https://cdn.tiny.cloud",
+   "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.adsterra.com https://*.effectivecpmnetwork.com https://*.adsterratrk.com https://*.adsterra.biz https://www.googletagmanager.com https://www.google-analytics.com https://cdn.tiny.cloud https://www.highperformanceformat.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: blob: https: http:",
   "font-src 'self' https://fonts.gstatic.com",
